f08587e28e28c86b52ce8e15ece4c1d0a3279686
hakenshi <fkdias025@gmail.com>
2025-05-16 18:50:59 -0300

    Adiciona novo componente Dialog e atualiza dependências. Melhora estilo do componente Hero e ajusta variantes de botão.

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -11,24 +11,16 @@ export default function Hero() {
                         <h1 className="text-5xl font-extrabold gray-gradient">
                             <span>Felipe Kafka Dias</span>
                         </h1>
-                        <p className="text-xl tracking-tight bg-radial from-slate-200 to-zinc-400 p-5 text-transparent bg-clip-text">Shall we build something cool together?</p>
-                    </div>
-                    <div className="space-y-3">
-                        <p>
-                            Hello. i'm a Fullstack Developer | Software Enginer, graduate in Information Technology from SENAC MG and currently pursuing a Bachelor's degree in Software Engineering at UNIFAE.
-                        </p>
-                        <p>I’m a dedicated professional with a critical mindset, always seeking to enhance my knowledge and skills in new technologies.</p>
-                        <p>
-                            I have experience in PHP, Laravel, React, and TailwindCSS, developing real systems and applying these technologies in challenging projects.
-                        </p>
-                        <p>I’m committed to delivering my projects efficiently, focusing on quality and continuous learning.</p>
+                        <p className="text-xl tracking-tight bg-radial from-slate-200 to-zinc-400 p-5 text-transparent bg-clip-text">Shall we
+                            <span className='bg-linear-to-t from-red-300 via-red-500 to-red-700 text-transparent bg-clip-text font-medium red-shadow'> build </span>
+                            something cool together?</p>
                     </div>
                     <div className='space-x-10 text-center mt-15'>
                         <Button size='lg' variant='defaultGlass' className='text-lg text-center w-56'>
                             Download My CV
                         </Button>
                         <Button size='lg' variant={'default'} className='text-lg text-center w-56 border border-red-600 hover:bg-slate-950/50 bg-slate-950/10 drop-shadow-red-500'>
-                            See More
+                            About Me
                         </Button>
                     </div>
                 </div>
